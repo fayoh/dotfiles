99c2fb362bcc5cf61926a6966d52c0dd5cc00fb3
Daniel Bengtsson <daniel.f.bengtsson@gmail.com>
2019-03-06 09:56:23 +0100

    Update path to sermon

--- site-specific/axis/zshrc.d/axis.zsh
+++ site-specific/axis/zshrc.d/axis.zsh
@@ -2,6 +2,6 @@
 . ~/.local/bin/oe-setup.sh > /dev/null
 
 #Debug cable
-alias debugcable="sudo sermon -f /dev/ttyUSB0"
+alias debugcable="sudo ~/.local/bin/sermon -f /dev/ttyUSB0"
 # Do I ever run xcam-scan other than eth0?
 alias camscan="xcam-scan eth0"
